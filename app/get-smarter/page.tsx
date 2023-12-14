6afab5e877d6540698c97b2481b2884e8c613157
John Rogers <jrhawktwo@gmail.com>
2023-12-14 06:37:17 -0600

    did inanother branch but needed to close issue lol

--- app/get-smarter/page.tsx
+++ app/get-smarter/page.tsx
@@ -14,27 +14,12 @@ import Loader from "../components/Loader";
 import InfiniteScroll from "../components/InfiniteScroll";
 
 import "react-toastify/dist/ReactToastify.css";
-import "./styles.css";
+import ToastIcon from "../components/ToastIcon";
 
 const FIRST_FETCH = 21;
 const VIDEOS_TO_FETCH = 9;
 
-const ToastIcon = () => {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="#7222C2"
-            className="w-8 h-8"
-        >
-            <path
-                fillRule="evenodd"
-                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-                clipRule="evenodd"
-            />
-        </svg>
-    )
-}
+
 
 function addExtraFields(submissionsList: any): any {
     return submissionsList.map((obj: any) => ({
